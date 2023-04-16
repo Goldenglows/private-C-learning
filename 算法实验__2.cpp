d5600fc653dae5f5af187cc542f94b22e1f01db9
Goldenglows <119770319+Goldenglows@users.noreply.github.com>
2023-04-16 12:47:19 +0800

    Add files via upload

---
+++ 算法实验__2.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(){
+    int num1,num2;
+
+    scanf("%d,%d",&num1,&num2);
+
+    //判断输入的数字是否大于1
+    if(num1>=1 && num2>=1){
+        if(num1==num2){
+            int k,sum=0;
+            for( k = 1; k<=num1-1;k++){
+                if(num1%k==0){
+                    sum +=k;
+                }
+            }
+            if(num1==sum)
+                printf("%d\n",num1);
+                printf("1");
+        }
+        else{
+        int i,j;
+        int m=0;
+        for( i = num1; i<=num2;i++){
+            int flag=0;
+            for( j=num1; j<i ;j++){
+                if(i%j==0){
+                    
+                    flag += j;
+                }
+            }
+            if ( i == flag){
+            printf("%d\n",i);
+            m++;
+            }
+            
+        }
+        printf("%d",m);
+        return 0;
+        }
+    }
+    else{
+        printf("输入错误");
+        return -1;
+    }
+
+
+}
