495e026b52b7a447b13449c26b3996e26e78a62b
Sapphirestar <119770319+Goldenglows@users.noreply.github.com>
2023-05-22 15:22:06 +0800

    Add files via upload

---
+++ 众数.cpp
@@ -0,0 +1,60 @@
+//int yuansu = arr[0];
+    //int flag = 1;
+    //int num = 1;
+    //int i;
+
+    //// 扫描序列并更新众数
+    //for ( i = 1; i < n; i++)
+    //{
+    //    if (arr[i] == arr[i - 1])
+    //        flag++;
+    //    else
+    //    {
+    //        if (flag > num)
+    //        {
+    //            num = flag;
+    //            yuansu = arr[i - 1];
+    //        }
+    //        flag = 1;
+    //    }
+    //}
+
+    //// 处理最后一个元素
+    //if (flag > num)
+    //    yuansu = arr[n - 1];
+
+    //// 输出众数
+    //printf("\n");
+    //flag = 0;
+    //for ( i = 0; i < n; i++)
+    //{
+    //    if (arr[i] == yuansu)
+    //    {
+
+    //        //printf("%d ", arr[i]);
+    //        flag++;
+    //    }
+    //}
+  
+    //if (flag == 0)
+    //    printf("不存在");
+
+    //int i, j, maxCount = 0, mode, count;
+
+    //printf("\n");
+
+    //for (i = 0; i < n; i++)
+    //{
+    //    count = 0;
+    //    for (j = 0; j < n; j++)
+    //    {
+    //        if (arr[j] == arr[i])
+    //            count++;
+    //    }
+    //    if (count > maxCount)
+    //    {
+    //        maxCount = count;
+    //        mode = arr[i];
+    //    }
+    //}
+    //printf("%d ", mode);
