1086d53b7288a69ab18880a34e8a682e66d8ffd3
Sapphirestar <119770319+Goldenglows@users.noreply.github.com>
2023-06-11 11:06:04 +0800

    Add files via upload

---
+++ 230611实验2.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#define maxnum 46 
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    // 初始化状态数组
+    int dp[maxnum+1]; 
+    dp[1] = 1;
+    dp[2] = 2;
+
+    int i;
+
+    // 递推计算状态数组
+    for (i = 3; i <= n; i++) {
+
+        dp[i] = dp[i-1] + dp[i-2];
+
+    }
+
+    // 输出结果
+    printf("%d\n", dp[n]);
+
+    return 0;
+}
