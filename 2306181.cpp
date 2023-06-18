bd6e04d69156dde00a9b3faac34d6661358b9d7a
Sapphirestar <119770319+Goldenglows@users.noreply.github.com>
2023-06-18 10:34:26 +0800

    Add files via upload

---
+++ 2306181.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+long long a,b,n,m,f[23],ma[23][23];
+
+void work(long long x,long long y){
+    ma[x][y]=1;
+    ma[x-1][y-2]=1;
+    ma[x-2][y-1]=1;
+    ma[x-2][y+1]=1;
+    ma[x-1][y+2]=1;
+    ma[x+1][y-2]=1;
+    ma[x+2][y-1]=1;
+    ma[x+2][y+1]=1;
+    ma[x+1][y+2]=1;
+}
+
+int main(){
+    scanf("%lld %lld %lld %lld",&n,&m,&a,&b);
+    
+    a++;
+    b++;
+    n++;
+    m++;
+    work(a,b);
+    x[1][1]=1;
+    
+    int i,j;
+    
+    for(i=1;i<=n;i++)
+        for(j=1;j<=m;j++){
+			if(i==1&&j==1)continue;
+        	if(ma[i][j]==0)f[j]+=f[j-1];
+        }
+    printf("%lld",f[m]);
+    return 0;
+}
