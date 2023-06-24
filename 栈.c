42c550d0a816fc2746715941a21aafcc7927e5a0
Sapphirestar <119770319+Goldenglows@users.noreply.github.com>
2023-06-24 09:13:28 +0800

    Add files via upload

--- 栈.c
+++ 栈.c
@@ -1,82 +1,102 @@
 #include <stdio.h>
-#include <stdlib.h>
+#define maxsize 5  // 页面框的大小
 
-#define MAXSIZE 5
-
-struct SqStack{
-    int * base;
-    int * top;
-    int stackSize;
+// 定义栈结构
+struct Stack {
+    int data[maxsize];
+    int top;
 };
 
-void InitStack(struct SqStack * s);
-int Push(struct SqStack * s, int data);
-void DispStack(struct SqStack * s);
-int GetTop(struct SqStack *s);
-int Pop(struct SqStack *s);
-
-int main(){
-	struct SqStack *s = (struct SqStack *)malloc(sizeof(struct SqStack));
-	InitStack(s); 
-	
-	for(int i = 0; i < 8; i++){
-		Push(s, i);
-	}
-	
-	DispStack(s);
-	
-	printf("Get top: %d\n", GetTop(s));
-	
-	for(int i = 0; i < 8; i++){
-		printf("Stack pop: %d\n", Pop(s));
-	}
-	
-	free(s);
-	return 0;
+// 初始化栈
+void initialize(struct Stack *s) {
+    s->top = -1;
 }
 
-
-
-void InitStack(struct SqStack * s){
-    int * p = (int *)malloc(MAXSIZE * sizeof(int));
-    s->base = p;
-    s->top = p;
-    s->stackSize = MAXSIZE;
+// 判断栈是否为空
+int isEmpty(struct Stack *s) {
+    return (s->top == -1);
 }
 
-int Push(struct SqStack *s, int data){
-	if(s->top - s->base == s->stackSize){
-		printf("Failed to push data:%d in stack, stack is full\n", data);
-		return -1;
-	}
-	
-	*s->top ++ = data;
-	
-	printf("Push data: %d in stack\n", data);
-	return 0;
+// 判断栈是否已满
+int isFull(struct Stack *s) {
+    return (s->top == maxsize - 1);
 }
 
-void DispStack(struct SqStack *s){
-	int * p = s->top;
-	while(p != s->base){
-		printf("%d\n", *--p);
-	}
+// 入栈操作
+void push(struct Stack *s, int value) {
+    if (isFull(s)) {
+        printf("栈已满，无法入栈\n");
+        return;
+    }
+    s->data[++(s->top)] = value;
 }
 
-int GetTop(struct SqStack *s){
-	if(s->top == s->base){
-		printf("Stack is empty\n");
-		return -1;
-	}
-	
-	return *(s->top - 1);
+// 出栈操作
+int pop(struct Stack *s) {
+    if (isEmpty(s)) {
+        printf("栈已空，无法出栈\n");
+        return -1;
+    }
+    return s->data[(s->top)--];
 }
 
-int Pop(struct SqStack *s){
-	if(s->top == s->base){
-		printf("Stack is empty\n");
-		return -1;
-	}
-	
-	return * --s->top;
+// LRU页面置换算法
+void LRU(int pages[], int n) {
+    struct Stack stack;
+    initialize(&stack);
+    int i, j, index;
+    int pages = 0;
+
+    // 遍历页面请求
+    for (i = 0; i < n; i++) {
+        index = pages[i];
+        
+        // 检查页面是否已在栈中
+        int found = 0;
+        for (j = stack.top; j >= 0; j--) {
+            if (stack.data[j] == index) {
+                found = 1;
+                break;
+            }
+        }
+
+        // 页面不在栈中，发生缺页中断，进行页面置换
+        if (!found) {
+            pages++;
+            
+            // 栈已满，移除最久未使用的页面
+            if (isFull(&stack)) {
+                int oldpage = pop(&stack);
+                printf("置换页面 %d\n", oldpage);
+            }
+            
+            // 将新页面加入栈顶
+            push(&stack, index);
+        }
+
+        // 输出当前页面状态
+        printf("当前页面状态：");
+        for (j = stack.top; j >= 0; j--) {
+            printf("%d ", stack.data[j]);
+        }
+        printf("\n");
+    }
+
+    printf("总缺页次数：%d\n", pages);
+}
+
+int main() {
+    int n;
+    printf("请输入页面请求序列长度：");
+    scanf("%d", &n);
+
+    int pages[n];
+    printf("请输入页面请求序列：");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &pages[i]);
+    }
+
+    LRU(pages, n);
+
+    return 0;
 }
